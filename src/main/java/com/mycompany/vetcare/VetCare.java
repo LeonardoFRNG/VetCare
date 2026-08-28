245407a8add33f408fc709254bd16a04e9e6c514
LeonardoFRNG <leonardojim321@gmail.com>
2026-08-28 18:12:25 -0500

    Creation of the class in charge of connecting to the database, implementation of a single class that creates the tunnel as required, to avoid memory leaks.

--- src/main/java/com/mycompany/vetcare/VetCare.java
+++ src/main/java/com/mycompany/vetcare/VetCare.java
@@ -4,13 +4,21 @@
 
 package com.mycompany.vetcare;
 
+import com.mycompany.vetcare.config.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author leonardodicaprio
  */
 public class VetCare {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");//DDDDD
+ public static void main(String[] args) {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            System.out.println("¡Conexión exitosa a MySQL, Leonardo!");
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: " + e.getMessage());
+        }
     }
 }
